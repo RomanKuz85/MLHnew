8ad830b0176844453739edbd4e8d5c97f9a93413
romikkuz <roma.kuzmitskij@bk.ru>
2020-11-03 11:44:39 -0500

    check create button

---
+++ test/smoke/mainFunctionality.js
@@ -0,0 +1,21 @@
+const sel = require ('../../data/selectors.json');
+
+describe('Checking the name functionality', function () {
+
+    describe('Happy path', function () {
+
+        it('TC-021 Create button is clickable after 1-4 are filled in', function () {
+            browser.url('');
+            const inputName = $(sel.name).setValue("LadyBug");
+            const inputGender = $$(sel.gender)[1].click();
+            const inputAge = $(sel.age).setValue(123);
+            const click = $(sel.storyClick).click();
+            const inputStory = $$(sel.storyType)[6].click();
+            const create = $(sel.submitButton).isEnabled();
+            browser.pause(3000);
+            expect(create).toEqual(true);
+        });
+
+    });
+
+})
