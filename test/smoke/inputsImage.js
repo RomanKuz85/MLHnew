6eead1736fb470e4413c29f15f60a0f1df29cb32
romikkuz <roma.kuzmitskij@bk.ru>
2020-11-11 22:08:52 -0500

    edded smoke(story, inputGenders)

---
+++ test/smoke/inputsImage.js
@@ -0,0 +1,17 @@
+const sel = require('../../data/selectors.json');
+const exp = require('../../data/expected.json');
+const data = require('../../data/testData.json');
+const uploadImageBrowser = require('../../helpers/uploadImageBrowser');
+const path = require('path');
+
+describe('Inputs for Image input field negative tests', function () {
+
+    it('A-060-2 Can not upload .pdf file', function () {
+        browser.url('');
+        uploadImageBrowser(data.imageChoice.testPDF);
+        const error = $(sel.imageError).isExisting();
+        expect(error).toEqual(true);
+    })
+
+})
+
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,13 +17,13 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/smoke/*.js',
-        //'./test/smoke/mainFunctionality.js',
+        //'./test/smoke/*.js',
+        './test/smoke/inputsImage.js',
         //'./test/regression/*.js'
     ],
      //Patterns to exclude.
     exclude: [
-        './test/smoke/labels.js'
+        //'./test/smoke/labels.js'
 
     ],
     //
