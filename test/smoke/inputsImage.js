87c9f7e2b39adeb5795b0305b3e0bc806aa72eec
RomanKuz85 <73574620+romankuz85@users.noreply.github.com>
2020-11-12 13:36:37 -0500

    add Inputs Image file

--- test/smoke/inputsImage.js
+++ test/smoke/inputsImage.js
@@ -6,19 +6,21 @@ const path = require('path');
 
 describe('Inputs for Image input field negative tests', function () {
 
-    it('A-060-2 Can not upload .pdf file', function () {
-        browser.url('');
+    it('A-060-2 Can not upload pdf. file', function () {
+        browser.url('https://qa-apps.netlify.app/app_my_hero');
         uploadImageBrowser(data.imageChoice.testPDF);
+        browser.pause(3000);
         const error = $(sel.imageError).isExisting();
         expect(error).toEqual(true);
-    })
+    });
 
-    it('A-060-3 Can not upload .pdf file', function () {
-        browser.url('');
+    it('A-060-2 Can not upload xlsx. file', function () {
+        browser.url('https://qa-apps.netlify.app/app_my_hero');
         uploadImageBrowser(data.imageChoice.testXLSX);
+        browser.pause(3000);
         const error = $(sel.imageError).isExisting();
         expect(error).toEqual(true);
-    })
+    });
 
-})
+});
 
